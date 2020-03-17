c618695bb0216b80fe155a8482090d42bfb4369b
Alexey-petr0v <alexey-petr0v@yandex.ru>
2020-03-17 13:12:35 +0700

    Configured the webpack-assembly

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const PATHS = {
+    pages: path.resolve(__dirname, "pages"),
+    dist: path.resolve(__dirname, "dist"),
+}
+
+module.exports = {
+  entry: {
+    bundle: PATHS.pages + "/index.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: PATHS.dist,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: PATHS.pages + '/index.pug',
+        filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+        template: PATHS.pages + '/colors-and-type.pug',
+        filename: 'colors-and-type.html'
+    }),
+    new MiniCssExtractPlugin({
+        filename: '[name].css',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          },
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+            pretty: true
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, PATHS.dist)
+  }
+}
