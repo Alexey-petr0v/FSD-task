c2aefe991fc659b33100e53df3faf23f4da67bf6
Alexey-petr0v <alexey-petr0v@yandex.ru>
2020-04-01 12:07:18 +0700

    Added modifier 'small' for block 'dropdown'

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 import './style.scss'
-import Cleave from 'cleave.js';
-import '../project.blocks/masked-text-field/masked-text-field.js'
+import Cleave from 'cleave.js'
 import '../project.blocks/dropdown/dropdown.js'
+import '../project.blocks/masked-text-field/masked-text-field.js'
 
