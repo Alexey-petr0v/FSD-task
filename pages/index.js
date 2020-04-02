fda0189f5d94ec756ad198192ade20ee18e42f3e
Alexey-petr0v <alexey-petr0v@yandex.ru>
2020-04-02 18:55:22 +0700

    The 'dropdown' block is divided into 2 blocks: 'dropdown' and 'list-counter', the 'field' block is fixed, the 'form-elements' page is fixed

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,6 @@
 import './style.scss'
 import Cleave from 'cleave.js'
 import '../project.blocks/dropdown/dropdown.js'
+import '../project.blocks/list-counter/list-counter.js'
 import '../project.blocks/masked-text-field/masked-text-field.js'
 
--- project.blocks/dropdown/dropdown.js
+++ project.blocks/dropdown/dropdown.js
@@ -1,180 +1,19 @@
-/* !!!!!!!!!!!!!!!!!!!!!! Старый dropdown !!!!!!!!!!!!!!!!!!!!! */
-
-/* -------------- Переменные блока dropdown -------------- */
-
-var dropdown_top = '.field__input_top',
-    dropdown_bottom = $('.field__input_bottom'),
-    dropdown_item = '.field__dropdown-item';
-    lenghtList = $(dropdown_item).length;
-
-// Генерация массива переменных на основе размера списка на странице
-var varItems = new Array(lenghtList);
-for (var i = 0; i < lenghtList; i++) {
-    varItems[i] = new Object ();
-    varItems[i].btnMinus = '.field__dropdown-item_amount' + i + ' .field__minus';
-    varItems[i].btnPlus = '.field__dropdown-item_amount' + i + ' .field__plus';
-    varItems[i].amount = '.field__dropdown-item_amount' + i + ' .field__minusAndPlus h3';
-}
-
-/* ------------------------------------------------------- */
-
-
-/* -------------- Реализация блока dropdown -------------- */
-
-// ---- MAIN ---- //
-
-// Предварительный запуск изменения внежнего вида кнопок
-editButtons();
-
 // Обработчик нажатия dropdown
+var dropdown_top = '.dropdown__top',
+    dropdown_bottom = $('.dropdown__bottom');
+
 $(dropdown_top).click (function view(){
-    apply (varItems);
+    //console.log("развернуть/свернуть");
     dropdown_bottom.slideToggle()
 });
 
-// Обработчики нажатия кнопок '-' и '+'
-$(varItems[0].btnMinus).click (function view(){ minus (varItems, 0) });
-$(varItems[1].btnMinus).click (function view(){ minus (varItems, 1) });
-$(varItems[2].btnMinus).click (function view(){ minus (varItems, 2) });
-$(varItems[0].btnPlus).click (function view(){ plus (varItems, 0) });
-$(varItems[1].btnPlus).click (function view(){ plus (varItems, 1) });
-$(varItems[2].btnPlus).click (function view(){ plus (varItems, 2) });
-
 // Обработчик кнопки 'применить'
-$('.field__apply').click (function view(){
-    apply (varItems);
+$('.dropdown-list__apply').click (function view(){
+    //console.log("применить");
     dropdown_bottom.slideToggle();
 });
 
 // Обработчик кнопки 'отменить'
-$('.field__clear').click (function view(){
-    clear();
+$('.dropdown-list__clear').click (function view(){
+    //console.log("отменить");
 });
-
-
-// ---- FUNCTIONS ---- //
-
-// Функции изменения внежнего вида кнопок
-function editButtons(){
-    for (var i = 0; i < lenghtList; i++) {
-        initButtons(varItems[i].amount, varItems[i].btnMinus);
-    }
-}
-function initButtons(amount, btnMinus) {
-    var value = parseInt($(amount).text());
-    if (value > 0) {
-        if ($(btnMinus).hasClass('field__circle_not-active')) {
-            $(btnMinus).removeClass('field__circle_not-active');
-        }
-    }
-    else if (value == 0){ $(btnMinus).addClass('field__circle_not-active') }
-}
-
-// Реализация кнопки '-'
-function minus (itemsObj, x) {
-    var value = parseInt($(itemsObj[x].amount).text());
-
-    if (value > 1) {
-        $(itemsObj[x].amount).text(function() {return value - 1;});
-        editButtons();
-    }
-    else if (value == 1){
-        $(itemsObj[x].amount).text(function() {return value - 1;});
-        editButtons();
-    }
-    totalAmount(itemsObj);
-}
-
-// Реализация кнопки '+'
-function plus (itemsObj, x) {
-    var value = parseInt($(itemsObj[x].amount).text());
-    $(itemsObj[x].amount).text(function() {return value + 1;});
-      
-    if ($(itemsObj[x].btnMinus).hasClass("field__circle_not-active")) {
-        $(itemsObj[x].btnMinus).removeClass('field__circle_not-active')
-    }
-    totalAmount(itemsObj);
-}
-
-// Функция вывода общей суммы гостей (число + слово)
-function totalAmount(itemsObject) {
-    var totalAmount = totalAmountNum(itemsObject);
-    $(dropdown_top + ' p').text(function() {
-        var allGuests = theGuests(totalAmount);
-        return totalAmount + allGuests;
-    });
-}
-
-// Функция подсчета общей суммы гостей (только число)
-function totalAmountNum(itemsObject) {
-    var totalAmount = 0, value = 0;
-    for (var i = 0; i < lenghtList; i++){
-        value = parseInt($(itemsObject[i].amount).text());
-        totalAmount += value
-    }
-    if (totalAmount == 0) {
-        $(".field__clear").text("");
-    }
-    else {
-        $(".field__clear").text("отменить");
-    }
-    return totalAmount;
-}
-
-// Функция вывода слов: "гость", "гостя" и "гостей"
-function theGuests(totalAmount) {
-    var text,
-    lastDigits;
-    lastDigits = totalAmount - (Math.trunc((totalAmount/100)) * 100);
-
-    switch (lastDigits){
-        case 1: case 21: case 31: case 41: case 51: case 61: case 71: case 81: case 91:
-            text = " гость";
-            break;
-        case 2: case 22: case 32: case 42: case 52: case 62: case 72: case 82: case 92:
-        case 3: case 23: case 33: case 43: case 53: case 63: case 73: case 83: case 93:
-        case 4: case 24: case 34: case 44: case 54: case 64: case 74: case 84: case 94:
-            text = " гостя";
-            break;
-        default:
-            text = " гостей";
-            break;
-    }
-    return text;
-}
-
-// Реализация кнопки 'применить'
-function apply (itemsObj) {
-    var out = new Array (2);
-        out[0] = totalAmountNum(itemsObj);
-        out[1] = outItems();
-    $("#total-amount").attr("value", out);
-    $(dropdown_top + ' p').text(totalAmount(itemsObj));
-    
-    // Поля массива out:
-        // 1 поле [общая сумма списка]
-        // 2 список с присваиваниями { [пункт1 [заголовок пункта1], [сумма пункта1]],  [пункт2 [заголовок пункта2], [сумма пункта2]] }
-
-    return out
-}
-function outItems() {
-    var outItems = new Array(lenghtList),
-        amountClass = '.field__dropdown-item_amount';
-    for (var i = 0; i < lenghtList; i++) {
-        outItems[i] = new Object ();
-        outItems[i].title = $(amountClass + i + ' > h3').text();
-        outItems[i].amount = $(varItems[i].amount).text();
-    }
-    return outItems
-}
-
-// Реализация кнопки 'отменить'
-function clear() {
-    for (var i = 0; i < lenghtList; i++) {
-        $(varItems[i].amount).text(0);
-    }
-    $("#total-amount").attr("value", '');
-    totalAmount(varItems);
-    editButtons();
-}
-/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
