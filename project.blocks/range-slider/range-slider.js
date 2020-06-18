0997cf7c13c6af31e7872f4481e2b8b166d5a6fa
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-06-18 18:55:22 +0700

    Fixed 'range-slider' block

--- project.blocks/range-slider/range-slider.js
+++ project.blocks/range-slider/range-slider.js
@@ -1,66 +1,113 @@
-//Make the DIV element draggagle:
-dragElement(document.getElementById(("mydiv1")));
-dragElement(document.getElementById(("mydiv2")));
+
+import {getID} from '../../pages/scripts/generate_ID.js'
+let radnom_set = new Set();
+let numberOfClasses = 0;
+
+// Функция подсчета количества элементов с классом dropdown на странице
+$('.range-slider').map(function sumOfClasses() {
+  numberOfClasses++
+  });
+
+
+// Массив идентификатов всех элементов с классами dropdown__top, dropdown__bottom
+let uniqueIDTop = new Array(numberOfClasses);
+
+// Установка случайных ID всем классам dropdown__top
+let iteration = 0;
+$('.range-slider').map(function() {
+  uniqueIDTop[iteration] = getID(12, radnom_set);
+  $(this).attr('id', uniqueIDTop[iteration]);
+
+  let ID_left = uniqueIDTop[iteration]+'_left';
+  $(this).find(".range-slider__limit_left").attr('id', ID_left);
+  $(this).find(".range-slider__limit_left").children().attr('id', ID_left+"header");
+  dragElement(document.getElementById((ID_left)));
+
+  let ID_right = uniqueIDTop[iteration]+'_right';
+  $(this).find(".range-slider__limit_right").attr('id', ID_right);
+  $(this).find(".range-slider__limit_right").children().attr('id', ID_right+"header")
+  dragElement(document.getElementById((ID_right)));
+
+  let ID_accent = uniqueIDTop[iteration]+'_accent';
+  $(this).find(".range-slider__accent").attr('id', ID_accent);
+
+  iteration = iteration + 1;
+});
+
+
+
+// 1. ОК Сгенерировать ID для каждого range-slider
+// 2. ОК Создать ID для limit_left и ID для limit_right:
+//    ID range-slider + '_left' / ID + '_right'
+// 3. Установить эти ID в js ниже
+// 4. Организовать добавление этих ID в HTML
+// 5. Исправить SCSS
+
 
 function dragElement(elmnt) {
+  let parent_id = $("#"+elmnt.id).parent().parent().attr("id");
+  let left_id = "#"+parent_id+"_left";
+  let right_id = "#"+parent_id+"_right";
+  let accent_id = "#"+parent_id+"_accent";
+
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    let set_pos1 = elmnt.offsetLeft - pos1;
-    let set_pos2 = elmnt.offsetTop - pos2;
 
-    // set the element's new position:
-    //elmnt.style.top = set_pos2 + "px";
-    // console.log("top: "+elmnt.style.top);
     let margin_left_elmnt = $("#"+elmnt.id).css("margin-left");
-        margin_left_elmnt = margin_left_elmnt.replace(/[\p\x]/g, '');
-        margin_left_elmnt = parseInt(margin_left_elmnt);
+    if (typeof margin_left_elmnt !== 'undefined') {
+      margin_left_elmnt = margin_left_elmnt.replace(/[\p\x]/g, '');
+      margin_left_elmnt = parseInt(margin_left_elmnt);
+    };
 
     // Если элемент расположен на линии
-    let mydiv1_margin_left = $("#mydiv1").css("margin-left").replace(/[\p\x]/g, '');
-    let mydiv2_margin_left = $("#mydiv2").css("margin-left").replace(/[\p\x]/g, '');
+    let mydiv1_margin_left = $(left_id).css("margin-left");
+    let mydiv2_margin_left = $(right_id).css("margin-left");
+
+    if ((typeof mydiv1_margin_left !== 'undefined')&&(typeof mydiv2_margin_left !== 'undefined')) {
+      mydiv1_margin_left = mydiv1_margin_left.replace(/[\p\x]/g, '');
+      mydiv2_margin_left = mydiv2_margin_left.replace(/[\p\x]/g, '');
+    };
+
     let mydiv_raznica = mydiv2_margin_left-mydiv1_margin_left;
 
+    let width_elmnt = $("#"+elmnt.id).css("width").replace(/[\p\x]/g, '');
+
     // ЕСЛИ ТОЧКИ СТОЛКНУЛИСЬ
-    if (mydiv_raznica <= ($("#mydiv1").css("width").replace(/[\p\x]/g, ''))){
-      let margin_left = $("#content").css("margin-left");
+    if (mydiv_raznica <= width_elmnt){
+      let margin_left = $(accent_id).css("margin-left");
           margin_left = margin_left.replace(/[\p\x]/g, '');
           margin_left = parseInt(margin_left);
-      let width = $("#content").css("width");
+      let width = $(accent_id).css("width");
           width = width.replace(/[\p\x]/g, '');
           width = parseInt(width);
 
-      if (elmnt.id == "mydiv2") {
+      if ("#"+elmnt.id == right_id) {
         $("#"+elmnt.id).css("margin-left", margin_left_elmnt+1)
-        $("#content").css("width", width+1);
+        $(accent_id).css("width", width+1);
       }
-      else if (elmnt.id == "mydiv1") {
+      else if ("#"+elmnt.id == left_id) {
         $("#"+elmnt.id).css("margin-left", margin_left_elmnt-1);
-        $("#content").css("margin-left", margin_left-1);
-        $("#content").css("width", width+1);
+        $(accent_id).css("margin-left", margin_left-1);
+        $(accent_id).css("width", width+1);
       }
     }
     // ЕСЛИ ТОЧКИ В ПРЕДЕЛАХ ЛИНИИ
@@ -70,60 +117,61 @@ function dragElement(elmnt) {
         margin_left_elmnt -= pos1;
         $("#"+elmnt.id).css("margin-left", margin_left_elmnt)
       // Изменение маргина и ширины выделения
-        if (elmnt.id == "mydiv2") {
-          let width = $("#content").css("width");
+        if ("#"+elmnt.id == (right_id)) {
+          let width = $(accent_id).css("width");
               width = width.replace(/[\p\x]/g, '');
               width = parseInt(width)-pos1;
-          $("#content").css("width", width);
+          $(accent_id).css("width", width);
           mydiv_id_flag = true
         }
-        else if (elmnt.id == "mydiv1") {
-          let width = $("#content").css("width");
+        else if ("#"+elmnt.id == (left_id)) {
+          let width = $(accent_id).css("width");
               width = width.replace(/[\p\x]/g, '');
               width = parseInt(width)+pos1;
-          let margin_left = $("#content").css("margin-left");
+          let margin_left = $(accent_id).css("margin-left");
               margin_left = margin_left.replace(/[\p\x]/g, '');
               margin_left = parseInt(margin_left)-pos1;
-          $("#content").css({
+          $(accent_id).css({
             "margin-left" : margin_left,
             "width" : width
           });
           mydiv_id_flag = false
         }
-        let $value1 = $("#range-slider-value1");
-        let $value2 = $("#range-slider-value2");
-        if (margin_left_elmnt > 242) {if (mydiv_id_flag){$value2.text("20 000")}else{$value1.text("20 000")}}
-        else if (margin_left_elmnt > 234) {if (mydiv_id_flag){$value2.text("19 000")}else{$value1.text("19 000")}}
-        else if (margin_left_elmnt > 226) {if (mydiv_id_flag){$value2.text("18 000")}else{$value1.text("18 000")}}
-        else if (margin_left_elmnt > 218) {if (mydiv_id_flag){$value2.text("17 000")}else{$value1.text("17 000")}}
-        else if (margin_left_elmnt > 210) {if (mydiv_id_flag){$value2.text("16 000")}else{$value1.text("16 000")}}
-        else if (margin_left_elmnt > 201) {if (mydiv_id_flag){$value2.text("15 000")}else{$value1.text("15 000")}}
-        else if (margin_left_elmnt > 192) {if (mydiv_id_flag){$value2.text("14 000")}else{$value1.text("14 000")}}
-        else if (margin_left_elmnt > 183) {if (mydiv_id_flag){$value2.text("13 000")}else{$value1.text("13 000")}}
-        else if (margin_left_elmnt > 174) {if (mydiv_id_flag){$value2.text("12 000")}else{$value1.text("12 000")}}
-        else if (margin_left_elmnt > 165) {if (mydiv_id_flag){$value2.text("11 000")}else{$value1.text("11 000")}}
-        else if (margin_left_elmnt > 156) {if (mydiv_id_flag){$value2.text("10 000")}else{$value1.text("10 000")}}
-        else if (margin_left_elmnt > 139) {if (mydiv_id_flag){$value2.text("9 000")}else{$value1.text("9 000")}}
-        else if (margin_left_elmnt > 122) {if (mydiv_id_flag){$value2.text("8 000")}else{$value1.text("8 000")}}
-        else if (margin_left_elmnt > 105) {if (mydiv_id_flag){$value2.text("7 000")}else{$value1.text("7 000")}}
-        else if (margin_left_elmnt > 88) {if (mydiv_id_flag){$value2.text("6 000")}else{$value1.text("6 000")}}
-        else if (margin_left_elmnt > 71) {if (mydiv_id_flag){$value2.text("5 000")}else{$value1.text("5 000")}}
-        else if (margin_left_elmnt > 55) {if (mydiv_id_flag){$value2.text("4 000")}else{$value1.text("4 000")}}
-        else if (margin_left_elmnt > 41) {if (mydiv_id_flag){$value2.text("3 000")}else{$value1.text("3 000")}}
-        else if (margin_left_elmnt > 27) {if (mydiv_id_flag){$value2.text("2 000")}else{$value1.text("2 000")}}
-        else if (margin_left_elmnt > 13) {if (mydiv_id_flag){$value2.text("1 000")}else{$value1.text("1 000")}}
-        else {if (mydiv_id_flag){$value2.text("0")}else{$value1.text("0")}}
+        let output1 = $("#"+parent_id).find("#range-slider-output1");
+        let output2 = $("#"+parent_id).find("#range-slider-output2");
+        let input = $("#"+parent_id).find("input");
+        if      (margin_left_elmnt > 242) {setValues(input, mydiv_id_flag, output1, output2, "20 000")}
+        else if (margin_left_elmnt > 234) {setValues(input, mydiv_id_flag, output1, output2, "19 000")}
+        else if (margin_left_elmnt > 226) {setValues(input, mydiv_id_flag, output1, output2, "18 000")}
+        else if (margin_left_elmnt > 218) {setValues(input, mydiv_id_flag, output1, output2, "17 000")}
+        else if (margin_left_elmnt > 210) {setValues(input, mydiv_id_flag, output1, output2, "16 000")}
+        else if (margin_left_elmnt > 201) {setValues(input, mydiv_id_flag, output1, output2, "15 000")}
+        else if (margin_left_elmnt > 192) {setValues(input, mydiv_id_flag, output1, output2, "14 000")}
+        else if (margin_left_elmnt > 183) {setValues(input, mydiv_id_flag, output1, output2, "13 000")}
+        else if (margin_left_elmnt > 174) {setValues(input, mydiv_id_flag, output1, output2, "12 000")}
+        else if (margin_left_elmnt > 165) {setValues(input, mydiv_id_flag, output1, output2, "11 000")}
+        else if (margin_left_elmnt > 156) {setValues(input, mydiv_id_flag, output1, output2, "10 000")}
+        else if (margin_left_elmnt > 139) {setValues(input, mydiv_id_flag, output1, output2, "9 000")}
+        else if (margin_left_elmnt > 122) {setValues(input, mydiv_id_flag, output1, output2, "8 000")}
+        else if (margin_left_elmnt > 105) {setValues(input, mydiv_id_flag, output1, output2, "7 000")}
+        else if (margin_left_elmnt > 88)  {setValues(input, mydiv_id_flag, output1, output2, "6 000")}
+        else if (margin_left_elmnt > 71)  {setValues(input, mydiv_id_flag, output1, output2, "5 000")}
+        else if (margin_left_elmnt > 55)  {setValues(input, mydiv_id_flag, output1, output2, "4 000")}
+        else if (margin_left_elmnt > 41)  {setValues(input, mydiv_id_flag, output1, output2, "3 000")}
+        else if (margin_left_elmnt > 27)  {setValues(input, mydiv_id_flag, output1, output2, "2 000")}
+        else if (margin_left_elmnt > 13)  {setValues(input, mydiv_id_flag, output1, output2, "1 000")}
+        else                              {setValues(input, mydiv_id_flag, output1, output2, "0")}
     }
 
     // ЕСЛИ ТОЧКИ СЛЕВА ОТ ЛИНИИ
     else if (margin_left_elmnt <= -1){
-      let width = $("#content").css("width");
+      let width = $(accent_id).css("width");
           width = width.replace(/[\p\x]/g, '');
           width = parseInt(width)-1;
-      let margin_left = $("#content").css("margin-left");
+      let margin_left = $(accent_id).css("margin-left");
           margin_left = margin_left.replace(/[\p\x]/g, '');
           margin_left = parseInt(margin_left)+1;
-      $("#content").css({
+      $(accent_id).css({
         "margin-left" : margin_left,
         "width" : width
       });
@@ -132,16 +180,31 @@ function dragElement(elmnt) {
 
     // ЕСЛИ ТОЧКИ СПРАВА ОТ ЛИНИИ
     else if (margin_left_elmnt >= 255){
-      let width = $("#content").css("width");
+      let width = $(accent_id).css("width");
           width = width.replace(/[\p\x]/g, '');
           width = parseInt(width);
-      $("#content").css("width", width);
+      $(accent_id).css("width", width);
       $("#"+elmnt.id).css("margin-left", margin_left_elmnt-1)
     }
   }
 
+  // Установка результата в инпут
+  function setValues(input, flag, out1, out2, val) {
+    if (flag) {
+      out2.text(val);
+      $(input).attr("data-output2", val);
+      let output1 = $(input).attr("data-output1");
+      $(input).attr("value", "{output1: "+output1+", output2: "+val+"}")
+    }
+    else {
+      out1.text(val);
+      $(input).attr("data-output1", val);
+      let output2 = $(input).attr("data-output2");
+      $(input).attr("value", "{output1: "+val+", output2: "+output2+"}")
+    }
+  }
+
   function closeDragElement() {
-    /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
   }
