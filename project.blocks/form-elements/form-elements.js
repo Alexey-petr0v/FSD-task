b788f5254de37c553deff70ae5a101c1fa839386
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-10-23 15:03:27 +0700

    Fixed fixed 'form-elements' block: added check for the presence of the 'form-elements' block; fixed 'dropdown' block: variables have been moved outside of the conditional if statement, which is used to display child blocks

--- project.blocks/form-elements/form-elements.js
+++ project.blocks/form-elements/form-elements.js
@@ -2,44 +2,49 @@
 
 import Paginator from '../paginator/paginator.js'
 
-// --- Переменные с интервалом первичного вывода элементов (пагинатор)
-let id = "#element_24",
-    amount_pages = 15,
-    paginator = new Paginator(id, amount_pages, "#paginator-next");
 
-// Text Field
-$( ".form-elements .text-field__elem" ).eq(1).attr( "id", "text-filled" );
-if (document.getElementById('text-filled')){
-    document.getElementById('text-filled').value = "This is pretty awesome"
+if ($(".form-elements").length) {
+    
+    // --- Переменные с интервалом первичного вывода элементов (пагинатор)
+    let id = "#element_24",
+        amount_pages = 15,
+        paginator = new Paginator(id, amount_pages, "#paginator-next");
+    
+    // Text Field
+    $( ".text-field__elem" ).eq(1).attr( "id", "text-filled" );
+    if (document.getElementById('text-filled')){
+        document.getElementById('text-filled').value = "This is pretty awesome"
+    }
+    $( ".text-field__elem" ).eq(1).trigger( "focus" );
+    
+    // dropdown
+    $( ".list-counter__top" ).eq(1).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(1).find(".list-counter__plus").eq(0).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(1).find(".list-counter__plus").eq(0).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(1).find(".list-counter__plus").eq(1).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(1).find(".list-counter__plus").eq(1).trigger( "click" );
+    $( ".list-counter__top" ).eq(1).trigger( "click" );
+    
+    // dropdown
+    $( ".list-counter__top" ).eq(2).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(2).find(".list-counter__plus").eq(0).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(2).find(".list-counter__plus").eq(0).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(2).find(".list-counter__plus").eq(1).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(2).find(".list-counter__plus").eq(1).trigger( "click" );
+    
+    // expandable checkbox list
+    $('.dropdown__elem_no-border .dropdown__top').eq(3).trigger( "click" );
+    $('.dropdown__elem_no-border .dropdown__top').eq(3).css('background', 'url("/images/to_close.svg") calc(100% - 13px) 50% no-repeat #ffffff');
+    
+    // Dropdown
+    $( ".list-counter__top" ).eq(3).trigger( "click" );
+    
+    // Dropdown
+    $( ".list-counter__top" ).eq(4).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(4).find(".list-counter__plus").eq(0).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(4).find(".list-counter__plus").eq(0).trigger( "click" );
+    $( ".list-counter__bottom" ).eq(4).find(".list-counter__plus").eq(1).trigger( "click" );
+    
+    // -----------------------------------------
+
 }
-$( ".form-elements .text-field__elem" ).eq(1).trigger( "focus" );
-
-// dropdown
-$( ".form-elements .list-counter__top" ).eq(1).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(1).find(".list-counter__plus").eq(0).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(1).find(".list-counter__plus").eq(0).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(1).find(".list-counter__plus").eq(1).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(1).find(".list-counter__plus").eq(1).trigger( "click" );
-$( ".form-elements .list-counter__top" ).eq(1).trigger( "click" );
-
-// dropdown
-$( ".form-elements .list-counter__top" ).eq(2).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(2).find(".list-counter__plus").eq(0).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(2).find(".list-counter__plus").eq(0).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(2).find(".list-counter__plus").eq(1).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(2).find(".list-counter__plus").eq(1).trigger( "click" );
-
-// expandable checkbox list
-$('.form-elements .dropdown__elem_no-border .dropdown__top').eq(3).trigger( "click" );
-$('.form-elements .dropdown__elem_no-border .dropdown__top').eq(3).css('background', 'url("/images/to_close.svg") calc(100% - 13px) 50% no-repeat #ffffff');
-
-// Dropdown
-$( ".form-elements .list-counter__top" ).eq(3).trigger( "click" );
-
-// Dropdown
-$( ".form-elements .list-counter__top" ).eq(4).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(4).find(".list-counter__plus").eq(0).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(4).find(".list-counter__plus").eq(0).trigger( "click" );
-$( ".form-elements .list-counter__bottom" ).eq(4).find(".list-counter__plus").eq(1).trigger( "click" );
-
-// -----------------------------------------
