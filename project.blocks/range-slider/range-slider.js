f9b1426d490ace6e95cf9c7634764861588fca89
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-11-26 22:06:59 +0700

    Fixed 'range-slider' block: made the effect of dragging sliders to be smooth; fixed jumping of values when quickly dragging sliders; removed default sliders from styles and added to script; fixed 'running away' of sliders from the cursor

--- project.blocks/range-slider/range-slider.js
+++ project.blocks/range-slider/range-slider.js
@@ -6,80 +6,105 @@ var right = document.getElementsByClassName("range-slider__limit_right")[0];
 var slider = $( ".range-slider__slider" );
 var offset = slider.offset();
 var sliderWidth = slider.width();
-//var lineWidth = $(".range-slider__line").width();
+var limitWidth = $(".range-slider__limit").width();
 
 let valueMin = -100;
 let valueMax = 100;
-let value = 0;
+let value1 = 10;
+let value2 = 60;
 let step = 10;
 
-
 let allUnits = Math.abs(valueMin) + Math.abs(valueMax);
-console.log("allUnits: "+allUnits);
-
+let allSteps = Math.trunc(allUnits / step);
 let unit = sliderWidth / allUnits;
-console.log("unit: "+unit);
 let stepPx = Math.round(step * unit);
-console.log("stepPx: "+stepPx);
-
-// ok 1. Определить сколько пикселей в одной единице
-// ok 2. Определить размер шага в пикселях
-// 3. Заменить проверку вывода "Point1MarginLeft % stepPx == 0" на уравнение:
-// (Point1MarginLeft / 13).беру-целое
-// нахожу половину от 13
-// если (Point1MarginLeft / 13) < (половины от 13)
-// переместить на Point1MarginLeft
-/// иначе ереместить на Point1MarginLeft+13
-// ..
-// сделать остановку захвата поинтера при клике на весь слайдер?
 
 
-let Point1MarginLeft = $(".range-slider__limit.range-slider__limit_left").css("margin-left").replace(/px/i, '');
-console.log("Point1MarginLeft: "+Point1MarginLeft)
+// делятся на 13 без остатка
+// 78
+// 91
 
-let Point2MarginRight = $(".range-slider__limit.range-slider__limit_right").css("margin-right").replace(/px/i, '');
-console.log("Point2MarginRight: "+Point2MarginRight)
-let Point2MarginLeft = sliderWidth - Point2MarginRight;
-console.log("Point2MarginLeft: "+Point2MarginLeft)
+// приближены к числам делимым на 13 без остатка
+// 81
+// 89
 
-left.onmousedown = function(e) { // 1. отследить нажатие
+// отдалены от чисел делимых на 13 без остатка
+// 84
+// 86
+
+console.log("делятся на 13 без остатка")
+console.log("78 % 13: "+(78 % 13))
+console.log("91 % 13: "+(91 % 13))
+console.log("приближены к числам делимым на 13 без остатка")
+console.log("81 % 13: "+(81 % 13))
+console.log("90 % 13: "+(90 % 13))
+console.log("отдалены от чисел делимых на 13 без остатка")
+console.log("84 % 13: "+(84 % 13))
+console.log("86 % 13: "+(86 % 13))
+
+
+
+function calcNumStep (value, valueMin, step) {
+  let i = 0;
+  let bufValue = valueMin;
+  while (bufValue != value) {
+    bufValue += step;
+    i++ 
+  }
+  return i
+}
+
+let numStep1 = calcNumStep (value1, valueMin, step);
+$(".range-slider__output_1").text(value1);
+let Point1MarginLeft = numStep1*stepPx;
+$(".range-slider__limit.range-slider__limit_left").css("margin-left", Point1MarginLeft+"px");
+
+let numStep2 = calcNumStep (value2, valueMin, step);
+$(".range-slider__output_2").text(value2);
+let Point2MarginLeft = numStep2*stepPx;
+let Point2MarginRight = sliderWidth - Point2MarginLeft;
+$(".range-slider__limit.range-slider__limit_right").css("margin-right", Point2MarginRight+"px");
+
+left.onmousedown = function(e) { // отследить нажатие
   moveAt_l(e);
   function moveAt_l(e) {
-    Point1MarginLeft = (e.clientX - offset.left);
-    if ((Point1MarginLeft % stepPx == 0)&&(Point1MarginLeft < (Point2MarginLeft - stepPx - 12 - 5))){
-      //lineWidth = $(".range-slider__accent").width();
-      //console.log("lineWidth: "+lineWidth, "Point1MarginLeft: "+Point1MarginLeft, ", Point2MarginLeft: "+Point2MarginLeft);
+    Point1MarginLeft = (e.clientX - offset.left)-(limitWidth/2);
+    if ((Point1MarginLeft % stepPx) < (stepPx / 2)) {
+      value1 = Math.trunc((valueMin + Point1MarginLeft / stepPx * step)/step)*step;
+      $(".range-slider__output_1").text(value1);
+    }
+    if (Point1MarginLeft < (Point2MarginLeft - stepPx - 12 - 5)) {
       $(".range-slider__limit.range-slider__limit_left").css("margin-left", Point1MarginLeft+"px");
     }
   }
-
-  // 3, перемещать по экрану
+  // перемещать по экрану
   ball.onmousemove = function(e) { moveAt_l(e); }
-
-  // 3. отследить окончание переноса
+  // отследить окончание переноса
   left.onmouseup = function() {
     ball.onmousemove = null;
     left.onmouseup = null;
   }
 }
 
-
-right.onmousedown = function(e) { // 1. отследить нажатие
+right.onmousedown = function(e) { // отследить нажатие
   moveAt_r(e);
   function moveAt_r(e) {
-    Point2MarginRight = (sliderWidth-(e.clientX - offset.left))-5;
+    Point2MarginRight = (sliderWidth-(e.clientX - offset.left))-(limitWidth/2);
     Point2MarginLeft = sliderWidth - Point2MarginRight;
-    if (Point2MarginLeft % stepPx == 0){
-      //lineWidth = $(".range-slider__accent").width();
-      //console.log("lineWidth: "+lineWidth, "Point2MarginRight: "+Point2MarginRight, ", Point2MarginLeft: "+Point2MarginLeft);
+    if (value2 == valueMax) {
+      $(".range-slider__limit.range-slider__limit_right").css("margin-right", "0px");
+    }
+    else if ((Point2MarginRight != 0)&&(Point1MarginLeft < (Point2MarginLeft - stepPx - 12 - 5))) {
       $(".range-slider__limit.range-slider__limit_right").css("margin-right", Point2MarginRight+"px");
     }
+    if ((Point2MarginLeft % stepPx) < (stepPx / 2)){
+      value2 = Math.trunc((valueMin + Point2MarginLeft / stepPx * step)/step)*step;
+      $(".range-slider__output_2").text(value2);
+    }
   }
-
-  // 3, перемещать по экрану
+  // перемещать по экрану
   ball.onmousemove = function(e) { moveAt_r(e); }
-
-  // 3. отследить окончание переноса
+  // отследить окончание переноса
   right.onmouseup = function() {
     ball.onmousemove = null;
     right.onmouseup = null;
